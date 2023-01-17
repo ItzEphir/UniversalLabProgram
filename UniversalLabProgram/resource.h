a77577a9b9589babdc3bb8aec4fb877503b394f7
ItzEphir <d.y.dvoryannikov@mail.ru>
2023-01-17 19:01:15 +0300

    continued

--- UniversalLabProgram/resource.h
+++ UniversalLabProgram/resource.h
@@ -14,12 +14,17 @@
 #define IDC_BUTTON1                     1000
 #define IDC_BUTTON2                     1001
 #define IDC_EDIT1                       1001
+#define IDC_EDIT_WAVELENGTH             1001
 #define IDC_BUTTON3                     1002
 #define IDC_RADIO1                      1002
-#define IDC_EDIT2                       1002
+#define IDC_EDIT_PERIOD                 1002
 #define IDC_BUTTON4                     1003
-#define IDC_EDIT3                       1003
-#define IDC_EDIT4                       1004
+#define IDC_EDIT_FBG1_INTENSITY         1003
+#define IDC_EDIT_FBG1_LENGTH            1004
+#define IDC_EDIT_FBG2_INTENSITY         1005
+#define IDC_EDIT_FBG2_LENGTH            1006
+#define IDC_EDIT7                       1007
+#define IDC_EDIT_SPACE                  1007
 
 // Next default values for new objects
 // 
